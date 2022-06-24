8e055de9d48b1646a041a3e87cfd0370658f95c9
jamesdan-stack <66017061+jamesdan-stack@users.noreply.github.com>
2022-06-24 06:49:58 +0100

    Add files via upload

---
+++ name.js
@@ -0,0 +1,7 @@
+var myName = "James Semilore"
+var height = "6inches"
+var myCountry = "Nigeria"
+
+var ans = "My name is " + myName + ", i'm from "+ myCountry + " and " + height +"tall"
+
+console.log(ans)
